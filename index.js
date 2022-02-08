2fff23e9bbd7bcf9fc52912e97f083229a45d2ae
Alexander Davis <alexander.phillip.davis@gmail.com>
2022-02-08 20:03:09 +0000

    serve static assets

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
-const express = require('express')
+const express = require('express');
 const app = express();
 const port = 3000;
+const path = require('path');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '/views'));
 
 app.get('/', (req, res) => {
-    console.log('Sup, yo')
-    res.render('')
+    console.log(`Sup, yo, dey be lookin' at this site...`);
+    res.render('home');
+})
+
+app.listen(port, () => {
+    console.log(`listening on port ${port}...`)
 })
