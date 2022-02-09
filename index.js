7fad15fc5d0df7a95672de03ca09ca14c182c6ab
Alexander Davis <alexander.phillip.davis@gmail.com>
2022-02-09 06:51:20 +0000

    title in header template linked to res.locals.title for each page

--- index.js
+++ index.js
@@ -10,21 +10,25 @@ app.set('views', path.join(__dirname, '/views'));
 
 app.get('/', (req, res) => {
     console.log(`Sup, yo, dey be lookin' at this site...`);
+    res.locals.title = 'Ingenious | home';    
     res.render('home');
 })
 
 app.get('/services', (req, res) => {
     console.log(`Dey lookin' at services page now, yo...`)
+    res.locals.title = 'Ingenious | services'; 
     res.render('services');
 })
 
 app.get('/about', (req, res) => {
     console.log(`Now dey lookin' at what you're all about...`)
+    res.locals.title = 'Ingenious | about'; 
     res.render('about');
 })
 
 app.get('/contact', (req, res) => {
     console.log(`Looks like dey might want to be gettin' in contact, yo...`)
+    res.locals.title = 'Ingenious | contact'; 
     res.render('contact')
 })
 
